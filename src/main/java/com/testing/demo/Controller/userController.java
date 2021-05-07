be1cd65f835e1037165401f876e9c6f45958f72b
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-05-07 23:48:26 +0530

    User Validation Backend done

--- src/main/java/com/testing/demo/Controller/userController.java
+++ src/main/java/com/testing/demo/Controller/userController.java
@@ -1,10 +1,14 @@
 package com.testing.demo.Controller;
+import com.testing.demo.Model.Item;
+import com.testing.demo.Model.LoginUser;
 import com.testing.demo.Model.User;
 import com.testing.demo.Repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 //Main REST Controller.
 
@@ -26,5 +30,25 @@ public class userController {
     }
 
     //Get User Method.
+    @PostMapping("/validate")
+    public ResponseEntity<?> validateUser (@RequestBody LoginUser user){
+        String message = "default";
+        try {
+            User object = userRepo.findUserByUsername(user.getUserName());
+            if(object != null) {
+                if(object.getPassWord1().contentEquals(user.getPassword())){
+                    message = "Valid User";
+                } else {
+                    message = "Wrong Password";
+                }
+            } else
+            {
+                message = "User Not Found";
+            }
+            return new ResponseEntity<>(message , HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage() , HttpStatus.OK);
+        }
 
+    }
 }
