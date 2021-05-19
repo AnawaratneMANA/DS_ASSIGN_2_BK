d910a890caca0311f8daf8e79a555d7aa7ba63a1
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-05-19 21:46:41 +0530

    Swagger configuration added

--- src/main/java/com/testing/demo/Controller/CreditCardController.java
+++ src/main/java/com/testing/demo/Controller/CreditCardController.java
@@ -1,12 +1,10 @@
 package com.testing.demo.Controller;
-
 import com.testing.demo.Model.CreditCardPayment;
 import com.testing.demo.Repository.CreditCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
